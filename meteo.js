2eec48bd2699da3b66ddd26756d5901d06a7e6cf
Natan Binisti <natanbinistilyon@gmail.com>
2023-11-15 21:24:00 +0100

    commit

--- meteo.js
+++ meteo.js
@@ -1,33 +1,59 @@
-let city = 'Paris';
-getTemperature(city);
+let ville = 'Lyon';
 
-let changeCity = document.querySelector('#changer')
-changeCity.addEventListener('click', () => {
-    city = prompt('Choisissez votre ville :');
-    getTemperature(city)
-})
 
-function getTemperature(city) {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=97ad485b6db9822d9a93cb34073d61f3&units=metric';
+recevoirTemperature(ville);
 
-    let request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.responseType = 'json';
-    request.send();
+function recevoirTemperature(ville) {
 
-    request.onload = () => {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                let reponse = request.response;
-                /* console.log(reponse); */
-                let temperature = reponse.main.temp;
-                /* console.log(temperature); */
-                let city = reponse.name;
 
-                document.querySelector('#ville').textContent = city;               document.querySelector('#temperature_label').textContent = temperature;
-            } else {
-                alert('Une erreur est survenue, réessayez plus tard !')
+    let appid = '5dd4a3cfb9e347e4e1b4e7fd61fe21dd';
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville +'&appid='+ appid + '&lang=fr&units=metric';
+
+    let requete = new XMLHttpRequest();
+
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
+
+    requete.onload = function() {
+        console.log(requete);
+        if (requete.readyState == XMLHttpRequest.DONE) {
+            if (requete.status === 200) {
+                let temp = requete.response.main.temp;
+                let ville =  requete.response.name;
+                let icone = requete.response.weather[0].icon;
+                let min = requete.response.main.temp_min;
+                let max = requete.response.main.temp_max;
+
+                let newDiv = document.createElement('div');
+                newDiv.innerHTML = '<p>Min : <span class="text-primary font-weight-bold">'+ min + '°C</span>' + '</p> <p>Max : <span class="text-danger font-weight-bold"> ' + max + '°C</span></p>';
+
+                let img = document.createElement('img');
+                img.src = 'https://openweathermap.org/img/wn/'+icone+'@2x.png';
+
+                let selectVille = document.querySelector('#ville')
+                selectVille.innerHTML = '<h2>' + ville + '</h2>';
+
+                selectVille.append(img);
+                selectVille.append(newDiv);
+
+                let selectTemp = document.querySelector('#temperature_label')
+                selectTemp.textContent = temp;
+            }
+            else {
+                alert('Un problème est intervenu, merci de ressaisir une nouvelle ville ou revenir plus tard.');
             }
         }
+
     }
 }
+
+let bouton = document.querySelector('#changer');
+bouton.addEventListener('click', () => {
+
+    let villeChoisie = ville;
+
+    villeChoisie = prompt('Quelle ville souhaitez-vous choisir ?');
+    recevoirTemperature(villeChoisie);
+})
